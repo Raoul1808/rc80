803ce498360b813a1a61801412fb077fb20401c2
Raoul1808 <raoulthegeek@gmail.com>
2024-04-19 00:31:53 +0200

    Begin CHIP-8 emulator implementation

--- rc80-core/src/lib.rs
+++ rc80-core/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub const MEMORY_SIZE: usize = 4096;
+pub const REGISTER_AMOUNT: usize = 16;
+pub const STACK_SIZE: usize = 16;
+
+pub struct System {
+    pub program_counter: u16,
+    pub stack_pointer: u8,
+    pub stack: [u16; STACK_SIZE],
+    pub memory: [u8; MEMORY_SIZE],
+    pub v_registers: [u8; REGISTER_AMOUNT],
+    pub i_register: u16,
+    pub time_register: u8,
+    pub sound_register: u8,
+}
+
+impl Default for System {
+    fn default() -> Self {
+        Self {
+            program_counter: 0x200,
+            stack_pointer: 0,
+            stack: [0; STACK_SIZE],
+            memory: [0; MEMORY_SIZE],
+            v_registers: [0; REGISTER_AMOUNT],
+            i_register: 0,
+            time_register: 0,
+            sound_register: 0,
+        }
+    }
+}
+
+impl System {
+    pub fn load(&mut self, program_bytes: &[u8]) {
+        println!("Memory length: {}", self.memory.len());
+        println!("Program length: {}", program_bytes.len());
+        let len = program_bytes.len() + 0x200;
+        println!("Calculated destination length: {}", len);
+        self.memory[0x200..len].copy_from_slice(program_bytes);
+    }
+
+    pub fn step(&mut self) {
+        let opcode = (self.memory[self.program_counter as usize] as u16) << 8
+            | self.memory[self.program_counter as usize + 1] as u16;
+        println!("Found opcode: {:#06x}", opcode);
+        self.program_counter += 2;
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
+mod test {
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn test_system() {}
 }
--- rc80-desktop/src/main.rs
+++ rc80-desktop/src/main.rs
@@ -1,6 +1,13 @@
-use rc80_core::add;
+use std::io;
+
+use rc80_core::System;
 
 fn main() {
-    println!("Hello, world!");
-    println!("727 + 1116 = {}", add(727, 1116));
+    let mut sys = System::default();
+    let bytes = include_bytes!("/home/mew/Downloads/1-chip8-logo.ch8");
+    sys.load(bytes);
+    loop {
+        sys.step();
+        io::stdin().read_line(&mut String::new()).unwrap();
+    }
 }
