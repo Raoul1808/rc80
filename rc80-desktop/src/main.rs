90b67a2d48c11ee4c921a3196073881dcabfb258
Raoul1808 <raoulthegeek@gmail.com>
2024-04-19 11:47:47 +0200

    Add frontend gui with eframe

--- rc80-desktop/src/main.rs
+++ rc80-desktop/src/main.rs
@@ -1,13 +1,38 @@
-use std::io;
+use eframe::egui;
 
 use rc80_core::System;
 
-fn main() {
-    let mut sys = System::default();
-    let bytes = include_bytes!("/home/mew/Downloads/1-chip8-logo.ch8");
-    sys.load(bytes);
-    loop {
-        sys.step();
-        io::stdin().read_line(&mut String::new()).unwrap();
+#[derive(Default)]
+struct EmuApp {
+    sys: System,
+}
+
+impl EmuApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let mut app = Self::default();
+        let bytes = include_bytes!("/home/mew/Downloads/1-chip8-logo.ch8");
+        app.sys.load(bytes);
+        app
     }
+}
+
+impl eframe::App for EmuApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello eframe!");
+            if ui.button("Step emulation").clicked() {
+                self.sys.step();
+            }
+        });
+    }
+}
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "rc80 Desktop",
+        native_options,
+        Box::new(|cc| Box::new(EmuApp::new(cc))),
+    )
+    .unwrap()
 }
